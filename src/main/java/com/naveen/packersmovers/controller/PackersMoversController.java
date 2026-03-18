2fff694650e7bb703741ef71c4cf0676d4ba5932
yogigowda567 <122065713+yogigowda567@users.noreply.github.com>
2026-03-18 18:20:41 +0530

    Update PackersMoversController.java

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -1,5 +1,5 @@
 package com.naveen.packersmovers.controller;
-import com.naveen.packersmovers.messageservice.MessageService;
+// import com.naveen.packersmovers.messageservice.MessageService;
 import com.naveen.packersmovers.model.Customer;
 import com.naveen.packersmovers.service.PackersMoversService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +13,8 @@ public class PackersMoversController {
         @Autowired
         private PackersMoversService userService;
                 
-        @Autowired
-        private MessageService messageService;
+       // @Autowired
+       // private MessageService messageService;
 
        
         @PostMapping("/customerDetails")
